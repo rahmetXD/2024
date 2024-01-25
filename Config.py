8b7fb7e93651561f01a25f9b5d613d136691f5e2
rahmetXD <144803190+rahmetXD@users.noreply.github.com>
2024-01-25 07:52:37 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,15 +2,15 @@ import os
 
 
 class Config():
-# LEXPER VE RAHMET TARAFINDAN BAŞTAN AŞŞAĞIYA DÜZELTİLİRMİŞTİR!
-
+    # Get these values from my.telegram.org
+    # https://my.telegram.org
     API_ID = int(os.environ.get("API_ID", "25114508"))
     API_HASH = os.environ.get("API_HASH", "993ccdfe81548dade420e81bcd6807ce")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6937121302:AAHrlN3iOfslpteQEOIu-_Q9WxtL9jYYdxY")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME" , "@laisTagger_bot")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6937121302:AAFOJenG6jTL69ZDUbOd10YFSqDZPVNvgUY")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME" , "laisTagger_bot")
     BOT_NAME = os.environ.get("BOT_NAME" , "Lais User Tagger")
     BOT_ID = int(os.environ.get("BOT_ID" , "6937121302"))
     SUDO_USERS = os.environ.get("SUDO_USERS" , "6308601705").split()
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT" , "laisbots")
     OWNER_ID = int(os.environ.get("OWNER_ID" , "6308601705"))
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME" , "@lusttqw")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME" , "lusttqw")
